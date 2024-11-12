38e615916bba762f92363436d7cd2d687b4a8f5b
MatheusConstantino <matheusconstantino57@gmail.com>
2024-11-12 12:26:25 -0300

    Primeiro commit: estrutura inicial do projeto

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        
+        if ($token = JWTAuth::attempt($credentials)) {
+            return response()->json(['token' => $token]);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
